4ccb943887914230ae1d30eced27c4a6a68470f5
shrub <shrub719@gmail.com>
2026-09-22 14:12:40 +0100

    some proper note drawing

--- src/renderer.c
+++ src/renderer.c
@@ -230,7 +230,15 @@ void r_populate_r_notes(Renderer* renderer, ATrack* track) {
     Note* seek = atomic_load(&track->seek);
     int ms = atomic_load(&track->ms);
 
-    for (int i = 0; i < 5; i++) {
+    renderer->r_notes[0].angle = 0.785;
+    renderer->r_notes[0].x = HORIZONTAL_PADDING;
+    renderer->r_notes[0].y = 100.0;
+    renderer->r_notes[1].angle = 0.785;
+    renderer->r_notes[1].x = HORIZONTAL_PADDING;
+    renderer->r_notes[1].y = 380.0;
+    renderer->n_notes = 2;
+
+    for (int i = 0; i < MAX_RNOTES; i++) {
         int note_ms = seek[i].ms;
         int ms_until = note_ms - ms;
         if (ms_until > FORWARD_TRACK_WIDTH * PIXELS_PER_MS) break;
@@ -238,11 +246,18 @@ void r_populate_r_notes(Renderer* renderer, ATrack* track) {
         int x = HORIZONTAL_PADDING + ms_until * PIXELS_PER_MS;
         int lane = seek[i].lane;
 
-        renderer->r_notes[i].angle = 0.785 + 0.2 * i;
-        renderer->r_notes[i].x = x;
-        renderer->r_notes[i].y = 100.0 + 280.0 * (lane == BOTTOM);
+        // outline
+        renderer->r_notes[2 + 2*i]. angle = 0.785 - 0.002 * ms_until;
+        renderer->r_notes[2 + 2*i].x = x;
+        renderer->r_notes[2 + 2*i].y = 100.0 + 280.0 * (lane == BOTTOM);
+
+        // note
+        renderer->r_notes[2 + 2*i + 1].angle = 0.785 - 0.001 * ms_until;
+        renderer->r_notes[2 + 2*i + 1].x = x;
+        renderer->r_notes[2 + 2*i + 1].y = 100.0 + 280.0 * (lane == BOTTOM);
+
+        renderer->n_notes += 2;
     }
-    renderer->n_notes = 5;
 }
 
 void r_draw(Renderer* renderer) {
